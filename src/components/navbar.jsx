c23f30fca29edb8edd17b106e118956c58ca95d1
Tanishqgarg002 <105201408+Tanishqgarg002@users.noreply.github.com>
2023-12-28 22:51:46 +0530

    resolved the href of logo and About of Navbar to #

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar(props) {
         className="container-fluid dark"
         style={{ color: props.mode === "dark" ? "white" : "black" }}
       >
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button
@@ -29,7 +29,7 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/about">
+              <a className="nav-link active" aria-current="page" href="#">
                 {props.about}
               </a>
             </li>
